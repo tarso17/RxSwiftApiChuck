94f932a39dd2ebddf69facb26132481c675499a7
Saulo <saulo170@hotmail.com>
2020-12-22 12:21:07 -0300

    Add FactTableViewCell

---
+++ RxSwiftApiChuck/FactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FactTableViewCell.swift
+//  RxSwiftApiChuck
+//
+//  Created by Saulo Oliveira on 22/12/20.
+//  Copyright © 2020 Saulo Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class FactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
