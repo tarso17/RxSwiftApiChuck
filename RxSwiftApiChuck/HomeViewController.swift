21aa76b85eedf9b97e39d8930799175837d41495
Saulo <saulo170@hotmail.com>
2020-12-22 12:25:58 -0300

    Link HomeViewController IBOutlets

--- RxSwiftApiChuck/HomeViewController.swift
+++ RxSwiftApiChuck/HomeViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var StatusLabel: UILabel!
+    @IBOutlet weak var statusView: UIView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.keyboardDismissMode = .onDrag
         // Do any additional setup after loading the view.
     }
 
