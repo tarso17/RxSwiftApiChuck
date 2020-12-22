4d59aed036be08810a6724ee15ddbcdd3db98dea
Saulo <saulo170@hotmail.com>
2020-12-22 16:31:25 -0300

    Loading state

--- RxSwiftApiChuck/HomeViewController.swift
+++ RxSwiftApiChuck/HomeViewController.swift
@@ -14,18 +14,32 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var statusView: UIView!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
     let disposeBag = DisposeBag()
+    let activityView = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+    
     private var viewModel: ChuckNorrisFactsListViewModel!
+    
     override func viewDidLoad() {
         print("ViewDidLoad")
         super.viewDidLoad()
+    viewModel.isLoading.drive(activityView.rx.isAnimating).disposed(by: disposeBag)
+        
         self.tableView.keyboardDismissMode = .onDrag
+        tableView.tableFooterView = UIView()
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellID)
         
+        activityView.center = self.view.center
+        activityView.color = .red
+        self.view.addSubview(activityView)
+        
+
         let searchResults = searchBar.rx.text.orEmpty
             .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
             .distinctUntilChanged()
             .flatMapLatest { query -> Observable<[ChuckNorrisFactViewModel]> in
+                self.activityView.startAnimating()
+                self.activityView.isHidden = false
                 if query.isEmpty {
                     self.searchBar.placeholder = "Search for a Chuck Norris Fact"
                     return self.viewModel.fetchFactViewModels(self.searchBar.text!)
@@ -36,6 +50,7 @@ class HomeViewController: UIViewController {
         
         searchResults.bind(to: tableView.rx.items(cellIdentifier: K.cellID, cellType: FactTableViewCell.self))  {
             (index, fact: ChuckNorrisFactViewModel, cell) in
+            self.activityView.stopAnimating()
             cell.bodyLabel.text = fact.body
             cell.categoryLabel.text = fact.category
             cell.shareButton.rx.tap.subscribe(onNext: { (_) in
