af95999f4b75a6e16238d775c5ee83f525023ed9
Saulo <saulo170@hotmail.com>
2020-12-22 14:49:37 -0300

    Add Constants struct

---
+++ RxSwiftApiChuck/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  RxSwiftApiChuck
+//
+//  Created by Saulo Oliveira on 22/12/20.
+//  Copyright © 2020 Saulo Oliveira. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let apiUrl = "https://api.chucknorris.io/jokes/"
+}
